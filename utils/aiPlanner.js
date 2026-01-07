2e47e4313606c0d27363d255096fdc5dfb0b5cd5
mohitsilavat09 <mohitsilavat24@gmail.com>
2026-01-07 16:36:15 +0530

    Update aiPlanner.js

--- utils/aiPlanner.js
+++ utils/aiPlanner.js
@@ -1,23 +1,14 @@
-// utils/aiPlanner.js
+export function autoSchedule(tasks) {
+  if (!tasks || tasks.length === 0) return [];
 
-export function autoSchedule(tasks = []) {
   let hour = 9;
 
   return tasks.map((task) => {
-    if (task.date) return task;
-
-    const date = new Date();
-    date.setHours(hour);
-    date.setMinutes(0);
-    date.setSeconds(0);
-
-    hour++;
-    if (hour > 18) hour = 9;
-
-    return {
+    const scheduledTask = {
       ...task,
-      date: date.toISOString(),
-      aiPlanned: true,
+      time: `${hour}:00`,
     };
+    hour++;
+    return scheduledTask;
   });
 }
